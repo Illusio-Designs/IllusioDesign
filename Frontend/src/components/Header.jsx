1a4317bbd997efdb2ff4d34ec40d23e51dbfdeda
agamnoor <noorahuja0910@gmail.com>
2024-10-10 13:20:51 +0530

    Header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,56 +1,80 @@
-// src/components/Header.jsx
-
 import React from 'react';
-import { motion } from 'framer-motion';
+import { color, motion } from 'framer-motion';
 import '../styles/Header.css'; // Import custom CSS for additional styling
 import Logo from '../assets/logo.png'
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="container">
-        {/* Logo Section */}
-        <motion.div
-          className="logo"
+    <>
+    <header className='py-3'>
+      <div className='container m-auto'>
+        <nav className='flex justify-between items-center'>
+        <motion.a
+          className="navbar-brand" href="/"
           initial={{ y: -10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
           <img
-            src={Logo}// Replace with your logo path
-            alt="Halo Lab Logo"
-            className="logo-image"
+            src={Logo}
+            alt="Illusio Designs"
+            className="img-fluid"
           />
-        </motion.div>
-
-        {/* Navigation Menu */}
-        <nav>
-          <ul className="nav-menu">
+        </motion.a>
+        <div className='flex items-center'>
+          <ul className="nav-links flex max-lg:absolute left-0 top-[-100%] max-lg:min-h-[60px] w-full lg:flex-row flex-col max-lg:gap-4 max-lg:p-5">
             <li>
-              <a href="#" className="nav-link">Home</a>
+              <motion.a 
+                initial="initial"
+                whileHover="hovered"
+                transition="duration"
+                className="text-md text-[#000] uppercase tracking-wider font-medium py-2 px-4 relative block whitespace-nowrap overflow-hidden" href="#">
+                <motion.div variants={{initial: {y: 0}, hovered: {y: "-120%"}, duration: 0.5 }}>About Us</motion.div>
+                <motion.div className='absolute inset-y-2 inset-x-4 hover:text-[#ec691f]' variants={{initial: {y: "120%"}, hovered: {y: 0}, duration: 0.5}}>About Us</motion.div>
+              </motion.a>
             </li>
             <li>
-              <a href="#" className="nav-link">About</a>
+              <motion.a 
+                initial="initial"
+                whileHover="hovered"
+                transition="duration"
+                className="text-md text-[#000] uppercase tracking-wider font-medium py-2 px-4 relative block whitespace-nowrap overflow-hidden" href="#">
+                <motion.div variants={{initial: {y: 0}, hovered: {y: "-120%"}, duration: 0.5 }}>Services</motion.div>
+                <motion.div className='absolute inset-y-2 inset-x-4 hover:text-[#ec691f]' variants={{initial: {y: "120%"}, hovered: {y: 0}, duration: 0.5}}>Services</motion.div>
+              </motion.a>
             </li>
             <li>
-              <a href="#" className="nav-link">Services</a>
+              <motion.a 
+                initial="initial"
+                whileHover="hovered"
+                transition="duration"
+                className="text-md text-[#000] uppercase tracking-wider font-medium py-2 px-4 relative block whitespace-nowrap overflow-hidden" href="#">
+                <motion.div variants={{initial: {y: 0}, hovered: {y: "-120%"}, duration: 0.5 }}>Blog</motion.div>
+                <motion.div className='absolute inset-y-2 inset-x-4 hover:text-[#ec691f]' variants={{initial: {y: "120%"}, hovered: {y: 0}, duration: 0.5}}>Blog</motion.div>
+              </motion.a>
             </li>
             <li>
-              <a href="#" className="nav-link">Blog</a>
-            </li>
-            <li>
-              <a href="#" className="nav-link">Contact</a>
+              <motion.a 
+                initial="initial"
+                whileHover="hovered"
+                transition="duration"
+                className="text-md text-[#000] uppercase tracking-wider font-medium py-2 px-4 relative block whitespace-nowrap overflow-hidden" href="#">
+                <motion.div variants={{initial: {y: 0}, hovered: {y: "-120%"}, duration: 0.5 }}>Contact Us</motion.div>
+                <motion.div className='absolute inset-y-2 inset-x-4 hover:text-[#ec691f]' variants={{initial: {y: "120%"}, hovered: {y: 0}, duration: 0.5}}>Contact Us</motion.div>
+              </motion.a>
             </li>
           </ul>
-        </nav>
-
-        {/* Call to Action Button */}
-        <div>
-          <a href="#" className="cta-button">Get Started</a>
         </div>
+        <div className='flex gap-6 items-center'>
+          <a href='#' className='btn btn-orange get-started'>Get Started</a>
+          <ion-icon name="menu" className="text-3xl bg-black cursor-pointer md:hidden" onclick="onToggleMenu(this)"></ion-icon>
+        </div>
+        </nav>
       </div>
     </header>
+    </>
   );
 };
 
 export default Header;
+
