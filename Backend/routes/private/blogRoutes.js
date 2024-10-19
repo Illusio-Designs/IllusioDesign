67711a736e0a789e3778a97d77cbc723e4603947
maharshibhogayata <88646267+maharshibhogayata@users.noreply.github.com>
2024-10-19 19:00:06 +0530

    updates of setu

--- Backend/routes/private/blogRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const blogController = require('../../controllers/Private/blogController');
-// const upload = require('../../config/multer');
-
-const router = express.Router();
-
-router.post('/create', blogController.createBlog);
-
-module.exports = router;
--- dashboard/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = import.meta.ILLUSIO_API_URL || 'http://localhost:3000/api';
-
-// export const API_BASE_URL = import.meta.ILLUSIO_API_URL || 'https://api.illusiodesigns.agency/api';
